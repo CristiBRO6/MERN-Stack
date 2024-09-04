9c82d8a912467746c1c57e1210bade8a5aa7ab1d
CristiBRO6 <brumacristian51@gmail.com>
2024-09-04 21:29:31 +0300

    edit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <>
-      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/dashboard/">Dashboard</Link>
     </>
   )
 }
