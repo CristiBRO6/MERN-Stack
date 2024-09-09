ef55b86097133a482bf59a155fefe036f2ab4ef8
CristiBRO6 <brumacristian51@gmail.com>
2024-09-09 16:56:06 +0300

    edit

--- client/src/components/ui/Checkbox.jsx
+++ client/src/components/ui/Checkbox.jsx
@@ -16,7 +16,7 @@ const Checkbox = ({ checked = false, disabled = false }) => {
       <button
         onClick={toggleChecked}
         className={`
-          w-4 h-4 flex items-center justify-center border rounded-md
+          w-4 h-4 flex items-center justify-center border rounded-[4px]
           ${isChecked ? 'bg-primary border-primary' : 'border-gray-400 bg-white'}
           ${disabled ? 'cursor-not-allowed opacity-50' : ''}
         `}
